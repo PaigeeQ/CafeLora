a9b67a48bfb62c2893a71fc7acf6a7de35dc2443
PaigeeQ <Pa.krajcova@gmail.com>
2025-05-03 13:47:38 +0200

    Opraveno Romano, pridany popisek a obrazek

--- src/components/Drink/index.jsx
+++ src/components/Drink/index.jsx
@@ -1,17 +1,27 @@
 import './style.css';
 
-export const Drink = ({ name, image }) => {
-  return (
-    <Drink
-    id={0}
-    name="Romano"
-    ordered={false}
-    image="./img/cups/romano.png"
-    layers={[
-      { color: '#fbdf5b', label: 'citrón' },
-      { color: '#613916', label: 'espresso' },
-    ]}
-  />
-  
-  );
-};
+export const Drink = ({ name, image, layers }) => {
+    return (
+      <>
+        <div className="drink">
+          <div className="drink__product">
+            <div className="drink__cup">
+              <img src={image} alt={name} />
+            </div>
+            <div className="drink__info">
+              <h3>{name}</h3>
+              {layers.map((layer, index) => (
+                <div className="layer" key={index}>
+                 <div className="layer__color" style={{ backgroundColor: layer.color }} />
+                 <div className="layer__label">{layer.label}</div>
+                </div>
+             ))}
+            </div>
+          </div>
+          <div className="drink__controls">
+            <button className="order-btn">Objednat</button>
+          </div>
+        </div>
+      </>
+    );
+  };
